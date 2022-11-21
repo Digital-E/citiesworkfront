d0e1081ee0de4aee5e112f517207c7bf8f45b9e3
SamuelBassett <samabassett@gmail.com>
2022-11-21 09:46:20 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 
 module.exports = {
-  swcMinify: false,
   images: {
     domains: ['cdn.sanity.io'],
-  },
-  compiler: {
-    styledComponents: true,
   },
   // async redirects() {
   //   return [
